2d72747046101b170af93d88e2c3f36de58af7fc
josiashod <djossoujosiashod@gmail.com>
2023-02-13 11:18:46 +0100

    Renommage de la classe Date en date

---
+++ src/date.cpp
@@ -0,0 +1,98 @@
+#include "date.h"
+
+date::date(){}
+
+date::date(unsigned int jour, unsigned int mois, unsigned int annee):
+    d_jour{jour}
+    , d_mois{mois}
+    , d_annee{annee}
+{}
+
+date::~date()
+{}
+
+unsigned int date::d_jour const
+{
+    return d_jour;
+}
+
+unsigned int date::d_mois const
+{
+    return d_mois;
+}
+
+unsigned int date::d_annee const
+{
+    return d_annee;
+}
+
+void date::setJour(unsigned int jour)
+{
+    d_jour = jour;
+}
+
+void date::setMois(unsigned int mois)
+{
+    d_mois = mois;
+}
+
+void date::setAnnee(unsigned int annee)
+{
+    d_annee = annee;
+}
+
+void lireDate(std::istream& ist)
+{
+    char c;
+    ist >> d_jour >> c >> d_mois >> c >> d_annee;
+}
+
+std::istream& operator>>(std::istream& ist, date& d)
+{
+    d.lireDate(ist);
+    return ist;
+}
+
+void date::afficheDate(std::ostream& ost) const
+{
+    ost << d_jour << "/" << d_mois << "/" << d_annee;
+}
+
+std::ostream& operator<<(std::ostream& ost, const date& d)
+{
+    d.afficheDate(ost);
+    return ost;
+}
+
+
+bool date::estAvant(const date& date) const
+{
+    if(d_annee > date.d_annee)
+    {
+        return false;
+    }
+    else if(d_annee == date.d_annee && d_mois > date.d_mois)
+    {
+        return false;
+    }
+    else if(d_annee == date.d_annee && d_mois == date.d_mois && d_jour >= date.d_jour)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool date::estMemeJourQue(const date& date) const
+{
+    return (d_annee == date.d_annee && d_mois == date.d_mois && d_jour == date.d_jour);
+}
+
+bool operator==(const date& date) const
+{
+    return estMemeJourQue(date);
+}
+
+bool operator>(const date& date) const
+{
+    return estAvant(date);
+}
---
+++ src/date.h
@@ -0,0 +1,39 @@
+#ifndef __DATE_H__
+#define __DATE_H__
+
+#include <iostream>
+
+
+class date
+{
+    public:
+        date();
+        date(unsigned int jour, unsigned int mois, unsigned int annee);
+        virtual ~date();
+
+        unsigned int jour() const;
+        unsigned int mois() const;
+        unsigned int annee() const;
+        void setAnnee(unsigned int annee);
+        void setMois(unsigned int mois);
+        void setJour(unsigned int jour);
+
+        void lireDate(istream& ist);
+        friend istream& operator>>(istream& ist, date& d);
+        void afficheDate(ostream& ost) const;
+        friend ostream& operator<<(ostream& ost, const date& d);
+
+        bool estAvant(const date& date) const;
+        bool estMemeJourQue(const date& date) const;
+
+        bool operator==(const date& date) const;
+        bool operator>(const date& date) const;
+
+
+    private:
+        unsigned int d_jour;
+        unsigned int d_mois;
+        unsigned int d_annee;
+};
+
+#endif // DATE_H
