00995c0c4af9ab47f06386732cb07224f3d3c167
josiashod <djossoujosiashod@gmail.com>
2023-02-14 13:06:36 +0100

    Separation de la structure des errors

---
+++ src/error.h
@@ -0,0 +1,12 @@
+#ifndef __ERROR_H__
+#define __ERROR_H__
+
+// Error 
+enum error {
+    OK                         = 0, // Everything is ok
+    ERR_NOT_FOUND              = 1, // Resource not found
+    ERR_WITH_VALUE             = 2, // Incorrect value
+};
+
+
+#endif
