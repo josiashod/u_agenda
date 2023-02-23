37c70a5d41ba741b721ee136258808fdf75d5792
josiashod <djossoujosiashod@gmail.com>
2023-02-23 17:38:13 +0100

    Ajout des evenement sur certains buttons

--- app/uagenda.cpp
+++ app/uagenda.cpp
@@ -1,16 +1,22 @@
 #include "uagenda.h"
 
+#include <QGridLayout>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QPushButton>
 #include <QLabel>
 #include <QLineEdit>
 #include <QCalendarWidget>
+#include <QTextCursor>
+#include <QFrame>
 
 UAgenda::UAgenda(QWidget *parent)
     : QWidget(parent)
+    , d_currentDate{QDate::currentDate()}
 {
     creerInterface();
+    afficheDate();
+    afficheCalendrier();
 }
 
 UAgenda::~UAgenda()
@@ -33,7 +39,6 @@ void UAgenda::creerInterface()
     auto search_button{new QPushButton(tr("chercher"))};
     auto today_button{new QPushButton(tr("Aujourd'hui"))};
     auto prev_button{new QPushButton(tr("<"))};
-    auto mode_button{new QPushButton(tr("Mois"))};
     auto next_button{new QPushButton(tr(">"))};
 
     header->addLayout(navbar, 2);
@@ -46,10 +51,15 @@ void UAgenda::creerInterface()
 
     navbar->addWidget(today_button, 0, Qt::AlignRight);
     navbar->addWidget(prev_button, 0, Qt::AlignRight);
-    navbar->addWidget(mode_button, 0, Qt::AlignRight);
     navbar->addWidget(next_button, 0, Qt::AlignRight);
     navbar->addLayout(searchbar, 0);
-    navbar->addWidget(new QLabel("17 Février 2023"), 0, Qt::AlignRight);
+
+    connect(today_button, &QPushButton::clicked, this, &UAgenda::onReinitDate);
+    connect(prev_button, &QPushButton::clicked, this, &UAgenda::onPrevMonth);
+    connect(next_button, &QPushButton::clicked, this, &UAgenda::onNextMonth);
+
+    d_etiquetteDate = new QLabel{""};
+    navbar->addWidget(d_etiquetteDate, 0, Qt::AlignRight);
     navbar->setSpacing(12);
     navbar->insertStretch( 0, -1 );
 
@@ -59,7 +69,8 @@ void UAgenda::creerInterface()
 
     // SIDEBAR
     auto create_button{new QPushButton(tr("+ Créer"))};
-    auto calendrier{new QCalendarWidget()};
+    d_calendrierWidget = new QCalendarWidget();
+    d_calendrierWidget->setGridVisible(true);
     auto search_contact{new QHBoxLayout()};
     auto search_contact_in{new QLineEdit()};
     auto search_contact_button{new QPushButton(tr("chercher"))};
@@ -70,22 +81,130 @@ void UAgenda::creerInterface()
     search_contact->addWidget(search_contact_button, 0);
     search_contact->setSpacing(0);
 
-    sidebar->addSpacing(30);
     sidebar->addWidget(create_button, 0, Qt::AlignTop);
-    sidebar->addWidget(calendrier, 0, Qt::AlignTop);
-    //sidebar->addWidget(new QLabel("UAGENDA"), 1);
+    sidebar->addWidget(d_calendrierWidget, 0, Qt::AlignTop);
     sidebar->addLayout(search_contact, 1);
-//    sidebar->setContentsMargins(10, 20 , 0, 10);
     sidebar->setSpacing(20);
     sidebar->insertStretch(-1, 1 );
-    //
-    //sidebar->addStretch(1);
+    connect(d_calendrierWidget, &QCalendarWidget::clicked, this, &UAgenda::onChangeDate);
 
+    auto calendrier{new QVBoxLayout()};
+    d_grille = new QGridLayout();
 
-    auto body{new QVBoxLayout()};
+    calendrier->addLayout(d_grille, 1);
     content->addLayout(sidebar, 0);
-    content->addLayout(body, 2);
+    content->addSpacing(10);
+    content->addLayout(calendrier, 1);
 
     main->addLayout(header, 0);
+    main->addSpacing(30);
     main->addLayout(content, 1);
+}
+
+void UAgenda::afficheDate()
+{
+    QLocale locale = QLocale();
+    d_etiquetteDate->setText(locale.toString(d_currentDate, "MMMM yyyy").toUpper());
+}
+
+
+void UAgenda::rafraichirGrille()
+{
+    QLayoutItem *child;
+    while ((child = d_grille->takeAt(0)) != nullptr)
+    {
+        delete child->widget(); // delete the widget
+        delete child;   // delete the layout item
+    }
+}
+
+void UAgenda::afficheCalendrier()
+{
+    rafraichirGrille();
+
+    int actual_day = 1;
+    QDate date{d_currentDate.year(), d_currentDate.month(), actual_day};
+    int row = 1;
+    d_grille->setRowStretch(row, 1);
+
+
+    for (int day = 1; day <= 7; ++day)
+    {
+        d_grille->setColumnStretch(day - 1, 1);
+        auto label {new QLabel(QLocale::system().dayName(day).toUpper())};
+        label->setStyleSheet("QLabel { font-weight: bold; margin-bottom: 10px }");
+        auto hligne {new QFrame{}};
+        hligne->setFrameStyle(QFrame::HLine|QFrame::Sunken);
+
+        d_grille->addWidget(label, 0, day - 1, Qt::AlignTop|Qt::AlignHCenter);
+        d_grille->addWidget(hligne, 0, day - 1, Qt::AlignBottom);
+    }
+
+    d_grille->setVerticalSpacing(10);
+
+    while(date.isValid())
+    {
+        int col = date.dayOfWeek();
+        auto label{new QLabel(QString::number(date.day()))};
+        label->setStyleSheet("QLabel { font-weight: bold; }");
+
+        auto vligne {new QFrame{}};
+        vligne->setFrameStyle(QFrame::VLine|QFrame::Sunken);
+        auto hligne {new QFrame{}};
+        hligne->setFrameStyle(QFrame::HLine|QFrame::Sunken);
+
+        d_grille->addWidget(vligne, row, col - 1, Qt::AlignLeft);
+        d_grille->addWidget(label, row, col - 1, Qt::AlignTop|Qt::AlignCenter);
+        d_grille->addWidget(hligne, row, col - 1, Qt::AlignBottom);
+        if (col == 7)
+        {
+            ++row;
+            d_grille->setRowStretch(row, 1);
+        }
+        ++actual_day;
+        date.setDate(d_currentDate.year(), d_currentDate.month(), actual_day);
+    }
+}
+
+void UAgenda::onChangeDate(QDate date)
+{
+    bool rafraichir{date.month() != d_currentDate.month()};
+
+    d_currentDate = date;
+    afficheDate();
+
+    if (rafraichir)
+        afficheCalendrier();
+}
+
+void UAgenda::onReinitDate()
+{
+    d_currentDate = QDate::currentDate();
+    d_calendrierWidget->setSelectedDate(d_currentDate);
+    afficheDate();
+    afficheCalendrier();
+}
+
+void UAgenda::onNextMonth()
+{
+    if (d_currentDate.month() == 12)
+        d_currentDate.setDate(d_currentDate.year() + 1, 1, 1);
+    else
+        d_currentDate.setDate(d_currentDate.year(), d_currentDate.month() + 1, 1);
+
+    d_calendrierWidget->setCurrentPage(d_currentDate.year(), d_currentDate.month());
+    afficheDate();
+    afficheCalendrier();
+}
+
+void UAgenda::onPrevMonth()
+{
+    if (d_currentDate.month() == 1)
+        d_currentDate.setDate(d_currentDate.year() - 1, 12, 31);
+    else
+        d_currentDate.setDate(d_currentDate.year(), d_currentDate.month() - 1, 1);
+
+    d_calendrierWidget->setCurrentPage(d_currentDate.year(), d_currentDate.month());
+    afficheDate();
+    afficheCalendrier();
 }
--- app/uagenda.h
+++ app/uagenda.h
@@ -2,6 +2,13 @@
 #define UAGENDA_H
 
 #include <QWidget>
+#include <QDate>
+
+class QVBoxLayout;
+class QGridLayout;
+class QLabel;
+class QComboBox;
+class QCalendarWidget;
 
 class UAgenda : public QWidget
 {
@@ -11,5 +18,21 @@ public:
     UAgenda(QWidget *parent = nullptr);
     ~UAgenda();
     void creerInterface();
+    void afficheDate();
+    void afficheCalendrier();
+    void rafraichirGrille();
+
+public slots:
+    void onChangeDate(QDate date);
+    void onReinitDate();
+    void onNextMonth();
+    void onPrevMonth();
+
+
+private:
+    QDate d_currentDate;
+    QGridLayout *d_grille;
+    QCalendarWidget *d_calendrierWidget;
+    QLabel *d_etiquetteDate;
 };
 #endif // UAGENDA_H
