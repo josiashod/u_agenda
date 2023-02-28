8a354691c9c71a440e3a0271cf79ccd1b2b02f99
josiashod <djossoujosiashod@gmail.com>
2023-03-01 00:50:19 +0100

    Ajout de la fonction d'exportation de rdv

--- src/rdv.cpp
+++ src/rdv.cpp
@@ -1,22 +1,27 @@
 #include "rdv.h"
 
 // CONSTRUCTEURS + DESTRUCTEUR
-
 rdv::rdv()
 {}
 
 rdv::rdv(std::string nom, date d, heure h_debut
-, heure h_fin): d_nom{nom}, d_date{d}
+, heure h_fin, std::string description
+, std::string localisation): d_nom{nom}, d_date{d}
     , d_horaires{h_debut, h_fin}
+    , d_description{description}
+    , d_localisation{localisation}
     , d_participants{nullptr}
     , d_suiv{nullptr}
     // , d_prec{nullptr}
 {}
 
 rdv::rdv(std::string nom, date d, heure h_debut
-, heure h_fin
+, heure h_fin, std::string description
+, std::string localisation
 , LPersonne* participants): d_nom{nom}, d_date{d}
     , d_horaires{h_debut, h_fin}
+    , d_description{description}
+    , d_localisation{localisation}
     , d_participants{participants}
     , d_suiv{nullptr}
     // , d_prec{nullptr}
@@ -74,6 +79,26 @@ void rdv::setHFin(heure fin)
     d_horaires[1] = fin;
 }
 
+std::string rdv::description() const
+{
+    return d_description;
+}
+
+void rdv::setDescription(std::string description)
+{
+    d_description = description;
+}
+
+std::string rdv::localisation() const
+{
+    return d_localisation;
+}
+
+void rdv::setLocalisation(std::string localisation)
+{
+    d_localisation = localisation;
+}
+
 LPersonne* rdv::participants() const
 {
     return d_participants;
@@ -88,15 +113,76 @@ void rdv::ajouterParticipant(personne*& pers){
     d_participants->ajouter(pers);
 }
 
-// void rdv::save(ostream& ost) const{
-//     ost << d_date << " : " << d_nom << endl;
-//     ost << d_horaires[0] << "-" << d_horaires[1] << endl;
-//     afficherParticipants(ost);
-// }
+void rdv::save(std::ostream& ost) const
+{
+    ost << "BEGIN:RDV" << std::endl;
+    ost << '{' + d_nom + ", " + d_description + ", "
+    + d_localisation + '}' << std::endl;
+    ost << d_date;
+    ost << d_horaires[0];
+    ost << d_horaires[1];
+    ost << *d_participants;
+    ost << "END:RDV" << std::endl;
+}
 
-// void rdv::load(istream& ist){
+void rdv::load(std::istream& ist)
+{
+    std::string ligne{""};
+    int ist_pos;
 
-// }
+    ist >> ligne;
+
+    if (ligne != "BEGIN:RDV")
+        return;
+
+    char c;
+    ist >> c >> d_nom >> d_description >> d_localisation;
+
+    d_nom = d_nom.substr(0, d_nom.length() - 1);
+    d_description = d_description.substr(0, d_description.length() - 1);
+    d_localisation = d_localisation.substr(0, d_localisation.length() - 1);
+
+    ist >> d_date;
+    ist >> d_horaires[0];
+    ist >> d_horaires[1];
+
+    d_participants = new LPersonne();
+    ist >> (*d_participants);
+
+    ist >> ligne;
+}
+
+std::string timestamp(date d, heure h)
+{
+    std::string tz{""};
+
+    tz += std::to_string(d.annee());
+    tz += std::to_string(d.mois());
+    tz += std::to_string(d.jour()) + "T";
+    tz += std::to_string(h.h());
+    tz += std::to_string(h.mn()) + "00Z";
+
+    return tz;
+}
+
+void rdv::exporter(std::ostream& ost) const
+{
+    ost << "BEGIN:EVENT" << std::endl;
+    ost << "DTSTART:" << timestamp(d_date, d_horaires[0]) << std::endl;
+    ost << "DTEND:" << timestamp(d_date, d_horaires[1]) << std::endl;
+    ost << "SUMMARY:" << d_nom << std::endl;
+    ost << "DESCRIPTION:\\n\\n" << d_description << "\\n";
+
+    personne *crt = d_participants->tete();
+    while(crt)
+    {
+        ost << crt->nomComplet() << "\\n";
+        crt = crt->suivant();
+    }
+    ost << std::endl;
+    ost << "LOCATION:" << d_localisation << std::endl;
+    ost << "END:EVENT" << std::endl;
+}
 
 bool rdv::estAvant(const rdv& r) const
 {
@@ -150,12 +236,12 @@ bool rdv::operator==(const rdv& r){
         *r.d_participants == *d_participants);
 }
 
-// ostream& operator<<(ostream& ost, const rdv r){
-//     r.affiche(ost);
-//     return ost;
-// }
+std::ostream& operator<<(std::ostream& ost, const rdv& r){
+    r.save(ost);
+    return ost;
+}
 
-// istream& operator>>(istream& ist, rdv& r){
-//     r.lire(ist);
-//     return ist;
-// }
+std::istream& operator>>(std::istream& ist, rdv& r){
+    r.load(ist);
+    return ist;
+}
--- src/rdv.h
+++ src/rdv.h
@@ -2,9 +2,8 @@
 #define __RDV_H__
 
 #include <string>
-#include <iostream>
 #include <array>
-#include "LPersonne.h"
+#include "lpersonne.h"
 #include "date.h"
 #include "heure.h"
 
@@ -15,9 +14,11 @@ class rdv
         // CONSTRUCTEURS + DESTRUCTEUR
         rdv();
         rdv(std::string nom, date d, heure h_debut
-        , heure h_fin);
+        , heure h_fin, std::string description
+        , std::string localisation);
         rdv(std::string nom, date d, heure h_debut
-        , heure h_fin
+        , heure h_fin, std::string description
+        , std::string localisation
         , LPersonne* participants);
         rdv(const rdv& r);
         virtual ~rdv();
@@ -31,21 +32,26 @@ class rdv
         void setHDebut(heure debut);
         heure h_fin() const;
         void setHFin(heure fin);
+        std::string description() const;
+        void setDescription(std::string description);
+        std::string localisation() const;
+        void setLocalisation(std::string localisation);
         LPersonne* participants() const;
         void setParticipants(LPersonne *participants);
 
         //METHODES
         void ajouterParticipant(personne*& pers);
         void save(std::ostream& ost) const;
-        // void load(std::istream& ist);
+        void load(std::istream& ist);
+        void exporter(std::ostream& ost) const;
         bool estAvant(const rdv& r) const;
         bool overlap(const rdv& r) const;
 
 
         bool operator>(const rdv& r);
         bool operator==(const rdv& r);
-        // friend std::ostream& operator <<(std::ostream& ost, const rdv& r);
-        // friend std::istream& operator >>(std::istream& ist, rdv& r);
+        friend std::ostream& operator <<(std::ostream& ost, const rdv& r);
+        friend std::istream& operator >>(std::istream& ist, rdv& r);
 
 
     protected:
@@ -54,6 +60,8 @@ class rdv
         std::string d_nom;
         date d_date;
         std::array<heure, 2> d_horaires;
+        std::string d_description;
+        std::string d_localisation;
         LPersonne *d_participants;
         rdv *d_suiv;
         //rdv* d_prec;
